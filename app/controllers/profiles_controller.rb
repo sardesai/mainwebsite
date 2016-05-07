be7e4c3343a9b99dd64a5ff71e52a535e00f9db8
Shardul <shardul@ssardesai.com>
2016-05-07 20:19:04 +0000

    get rid of coffee script errors, add profile specific 404 and edit robots.txt

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,7 +1,9 @@
 class ProfilesController < ApplicationController
   def show
     if (!File.exists?("#{Rails.root}/filestore/profiles/"+params[:id]+".yaml"))
-      raise ActionController::RoutingError.new('Profile Not yet made')
+      #raise ActionController::RoutingError.new('Profile Not yet made')
+      render file: "public/profile_missing.html"
+      return
     end
     @profile = YAML.load_file("#{Rails.root}/filestore/profiles/"+params[:id]+".yaml");
     p @profile
